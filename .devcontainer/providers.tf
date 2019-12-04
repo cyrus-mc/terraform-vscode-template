6f36efb1ff99d926110e58d1d80a0d5537957954
Matthew Ceroni <matthew.ceroni@dat.com>
2019-12-04 11:50:23 -0800

    Initial

---
+++ .devcontainer/providers.tf
@@ -0,0 +1,11 @@
+provider "aws" {}
+provider "azurerm" {}
+provider "google" {}
+provider "alicloud" {}
+provider "helm" {}
+provider "kubernetes" {}
+provider "random" {}
+provider "null" {}
+provider "external" {}
+provider "template" {}
+provider "archive" {}
---
+++ output.tf
@@ -0,0 +1,1 @@
+# define outputs here
