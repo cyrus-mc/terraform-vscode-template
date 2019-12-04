d2efee89fad01e4b7dfdc6a451b926d7007f13af
cyrus-mc <cyrus-mc@users.noreply.github.com>
2019-12-04 14:47:04 -0800

    Initial Development (#1)
    Initial release

---
+++ .devcontainer/providers.tf
@@ -0,0 +1,11 @@
+provider "aws" { region = "us-west-2" }
+provider "azurerm" {}
+provider "google" {}
+provider "alicloud" {}
+provider "helm" {}
+provider "kubernetes" {}
+provider "random" {}
+provider "null" {}
+provider "external" {}
+provider "template" {}
+provider "archive" {}
